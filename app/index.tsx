25fc7d2b173bff7a6b2c64f557cafeb4eebeefab
adrianszablowski <adrian.szablowski.kontakt@gmail.com>
2024-12-10 21:48:35 +0100

    remove assets

--- app/index.tsx
+++ app/index.tsx
@@ -2,8 +2,8 @@ import { Text, View } from 'react-native';
 
 export default function Index() {
 	return (
-		<View className='flex justify-center items-center'>
-			<Text>Edit app/index.tsx to edit this screen.</Text>
+		<View className='flex justify-center items-center h-screen'>
+			<Text>Edit app/index.tsx to edit this screen!</Text>
 		</View>
 	);
 }
