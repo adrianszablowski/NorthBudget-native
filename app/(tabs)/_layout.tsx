6ee77eb3d4e7b1fc5505b86f250f8de8a2901291
adrianszablowski <adrian.szablowski.kontakt@gmail.com>
2024-12-11 22:27:59 +0100

    feat: add custom roboto fonts

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,4 +1,5 @@
 import { HapticTab } from '@/components/ui/HapticTab';
+import { AntDesign, Ionicons } from '@expo/vector-icons';
 import { Tabs } from 'expo-router';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
@@ -15,9 +16,24 @@ export default function TabLayout() {
 				tabBarButton: HapticTab,
 				tabBarStyle: Platform.select({ ios: { position: 'absolute' }, default: {} }),
 			}}>
-			<Tabs.Screen name='index' options={{ title: t('Dashboard') }} />
-			<Tabs.Screen name='expenses' options={{ title: t('Expenses') }} />
-			<Tabs.Screen name='(profile)' options={{ title: t('Profile') }} />
+			<Tabs.Screen
+				name='index'
+				options={{
+					title: t('Dashboard'),
+					tabBarIcon: ({ color }) => <AntDesign name='home' color={color} size={20} />,
+				}}
+			/>
+			<Tabs.Screen
+				name='expenses'
+				options={{
+					title: t('Expenses'),
+					tabBarIcon: ({ color }) => <AntDesign name='creditcard' color={color} size={20} />,
+				}}
+			/>
+			<Tabs.Screen
+				name='(profile)'
+				options={{ title: t('Profile'), tabBarIcon: ({ color }) => <AntDesign name='user' color={color} size={20} /> }}
+			/>
 		</Tabs>
 	);
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,39 @@
-import { Stack } from 'expo-router';
+import { SplashScreen, Stack } from 'expo-router';
 import '@/localization/i18n';
-
 import '../global.css';
 import { StatusBar } from 'expo-status-bar';
+import { useFonts } from 'expo-font';
+import { useEffect } from 'react';
+import { View } from 'react-native';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+	const [loaded, error] = useFonts({
+		RobotoRegular: require('@/assets/fonts/Roboto-Regular.ttf'),
+		RobotoThin: require('@/assets/fonts/Roboto-Thin.ttf'),
+		RobotoThinItalic: require('@/assets/fonts/Roboto-ThinItalic.ttf'),
+		RobotoMedium: require('@/assets/fonts/Roboto-Medium.ttf'),
+		RobotoMediumItalic: require('@/assets/fonts/Roboto-MediumItalic.ttf'),
+		RobotoLight: require('@/assets/fonts/Roboto-Light.ttf'),
+		RobotoLightItalic: require('@/assets/fonts/Roboto-LightItalic.ttf'),
+		RobotoItalic: require('@/assets/fonts/Roboto-Italic.ttf'),
+		RobotoBold: require('@/assets/fonts/Roboto-Bold.ttf'),
+		RobotoBoldItalic: require('@/assets/fonts/Roboto-BoldItalic.ttf'),
+		RobotoBlack: require('@/assets/fonts/Roboto-Black.ttf'),
+		RobotoBlackItalic: require('@/assets/fonts/Roboto-BlackItalic.ttf'),
+	});
+
+	useEffect(() => {
+		if (loaded || error) {
+			SplashScreen.hideAsync();
+		}
+	}, [loaded, error]);
+
+	if (!loaded && !error) {
+		return null;
+	}
+
 	return (
 		<>
 			<Stack>
