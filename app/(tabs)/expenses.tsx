ff4bce91cea64908d0786bc9e2b6727fc38d84f2
adrianszablowski <adrian.szablowski.kontakt@gmail.com>
2024-12-15 21:44:43 +0100

    feat: create data table and installed lodash

--- app/(tabs)/expenses.tsx
+++ app/(tabs)/expenses.tsx
@@ -1,11 +1,8 @@
 import React from "react";
-import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Expenses() {
   return (
-    <SafeAreaView className="h-screen bg-background-0 px-3">
-      <Text>Expenses</Text>
-    </SafeAreaView>
+    <SafeAreaView className="h-screen bg-background-0 px-3"></SafeAreaView>
   );
 }
