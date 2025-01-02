91acf8483ab93221666d81b5e6daaf35e1d671a2
adrianszablowski <adrian.szablowski.kontakt@gmail.com>
2025-01-02 11:55:12 +0100

    feat: add switch from gluestack, add missing imputs in expense form and add translations

--- components/expenses/exepense-form.tsx
+++ components/expenses/exepense-form.tsx
@@ -1,8 +1,24 @@
 import { useRouter } from "expo-router";
 import React from "react";
 import { useTranslation } from "react-i18next";
+import colors from "tailwindcss/colors";
 import { Button, ButtonText } from "../ui/button";
+import { HStack } from "../ui/hstack";
+import { ChevronDownIcon } from "../ui/icon";
 import { Input, InputField } from "../ui/input";
+import {
+  Select,
+  SelectBackdrop,
+  SelectContent,
+  SelectDragIndicator,
+  SelectDragIndicatorWrapper,
+  SelectIcon,
+  SelectInput,
+  SelectItem,
+  SelectPortal,
+  SelectTrigger,
+} from "../ui/select";
+import { Switch } from "../ui/switch";
 import { Text } from "../ui/text";
 import { VStack } from "../ui/vstack";
 
@@ -13,19 +29,52 @@ export default function ExpenseForm() {
   return (
     <VStack space="md" className="w-full max-w-[500px]">
       <VStack space="xs">
-        <Text className="text-typography-500">{t("Email")}</Text>
+        <Text className="text-typography-500">{t("Title")}</Text>
         <Input>
           <InputField type="text" />
         </Input>
       </VStack>
       <VStack space="xs">
-        <Text className="text-typography-500">{t("Password")}</Text>
+        <Text className="text-typography-500">{t("Amount")}</Text>
         <Input>
-          <InputField type="password" />
+          <InputField type="text" keyboardType="numeric" />
         </Input>
       </VStack>
+      <VStack space="xs">
+        <Text className="text-typography-500">{t("Category")}</Text>
+        <Select>
+          <SelectTrigger variant="outline" size="md">
+            <SelectInput placeholder={t("Select category")} />
+            <SelectIcon className="mr-3" as={ChevronDownIcon} />
+          </SelectTrigger>
+          <SelectPortal>
+            <SelectBackdrop />
+            <SelectContent className="pb-10">
+              <SelectDragIndicatorWrapper>
+                <SelectDragIndicator />
+              </SelectDragIndicatorWrapper>
+              <SelectItem label="Car" value="1" />
+              <SelectItem label="Home" value="2" />
+            </SelectContent>
+          </SelectPortal>
+        </Select>
+      </VStack>
+      <HStack space="md">
+        <Switch
+          trackColor={{ false: colors.gray[300], true: colors.blue[500] }}
+          ios_backgroundColor={colors.gray[300]}
+        />
+        <Text size="sm">{t("Paid")}</Text>
+      </HStack>
+      <HStack space="md">
+        <Switch
+          trackColor={{ false: colors.gray[300], true: colors.blue[500] }}
+          ios_backgroundColor={colors.gray[300]}
+        />
+        <Text size="sm">{t("Standing order")}</Text>
+      </HStack>
       <Button onPress={() => push("/dashboard")}>
-        <ButtonText>{t("Sign in")}</ButtonText>
+        <ButtonText>{t("Submit")}</ButtonText>
       </Button>
     </VStack>
   );
