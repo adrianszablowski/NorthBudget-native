a1d34cc9423cebdfada4c331927ebbaaaa9bec9c
adrianszablowski <adrian.szablowski.kontakt@gmail.com>
2025-01-06 15:54:11 +0100

    refacotr: remove useTranslation from dasboard

--- app/(tabs)/dashboard.tsx
+++ app/(tabs)/dashboard.tsx
@@ -4,12 +4,9 @@ import ExpensesChart from "@/components/dashboard/expenses-chart";
 import TotalCurrentExpensesCard from "@/components/dashboard/total-current-expenses-card";
 import UpcomingExpensesCard from "@/components/dashboard/upcoming-expenses-card";
 import { VStack } from "@/components/ui/vstack";
-import { useTranslation } from "react-i18next";
 import { SafeAreaView, ScrollView } from "react-native";
 
 export default function DashboardPage() {
-  const { t } = useTranslation();
-
   return (
     <SafeAreaView className="h-full bg-background-0">
       <ScrollView className="px-3 py-2">
