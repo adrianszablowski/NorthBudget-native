d4427a212256443647d0c5c289f22f87db40f9ba
adrianszablowski <adrian.szablowski.kontakt@gmail.com>
2025-01-12 20:25:55 +0100

    feat: add translations to schema

--- schemas/schema.ts
+++ schemas/schema.ts
@@ -1,19 +1,25 @@
+import i18next from "i18next";
 import { z } from "zod";
 
 export const signUpFormSchema = z
   .object({
-    email: z.string().email().min(1, { message: "Email is required" }),
-    username: z.string().min(1, { message: "Username is required" }),
-    password: z.string().min(1, { message: "Password is required" }),
+    email: z
+      .string()
+      .email({ message: i18next.t("Email is invalid") })
+      .min(1, { message: i18next.t("Email is required") }),
+    username: z.string().min(1, { message: i18next.t("Username is required") }),
+    password: z
+      .string()
+      .min(6, { message: i18next.t("Password must have at least 6 letters") }),
     repeatPassword: z
       .string()
-      .min(1, { message: "Passwords must be the same" }),
+      .min(1, { message: i18next.t("Passwords must be the same") }),
   })
   .superRefine(({ password, repeatPassword }, ctx) => {
     if (repeatPassword !== password) {
       ctx.addIssue({
         code: "custom",
-        message: "Passwords must be the same",
+        message: i18next.t("Passwords must be the same"),
         path: ["repeatPassword"],
       });
     }
