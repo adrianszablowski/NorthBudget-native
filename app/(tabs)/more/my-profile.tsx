639e628c017c5de2c34cd9da82cdc74b7d5b3872
adrianszablowski <adrian.szablowski.kontakt@gmail.com>
2025-01-16 22:49:50 +0100

    feat: show user avatar and username

--- app/(tabs)/more/my-profile.tsx
+++ app/(tabs)/more/my-profile.tsx
@@ -1,10 +1,5 @@
 import UserDetailsForm from "@/components/my-profile/user-details-form";
-import {
-  Avatar,
-  AvatarBadge,
-  AvatarFallbackText,
-  AvatarImage,
-} from "@/components/ui/avatar";
+import { Avatar, AvatarBadge, AvatarImage } from "@/components/ui/avatar";
 import { Divider } from "@/components/ui/divider";
 import { Heading } from "@/components/ui/heading";
 import { HStack } from "@/components/ui/hstack";
@@ -24,17 +19,16 @@ export default function MyProfile() {
       <VStack space="lg" className="px-3 py-2">
         <HStack space="md">
           <Avatar size="xl">
-            <AvatarFallbackText>{user?.name}</AvatarFallbackText>
             <AvatarImage
               source={{
-                uri: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+                uri: user?.avatar,
               }}
             />
             <AvatarBadge />
           </Avatar>
           <VStack>
             <Text size="2xl" bold>
-              {user?.name}
+              {user?.username}
             </Text>
             <Text className="text-typography-500">{user?.email}</Text>
           </VStack>
