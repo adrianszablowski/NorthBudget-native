8f48ec13875eeceadfc77de88707a574e4712a0f
adrianszablowski <adrian.szablowski.kontakt@gmail.com>
2025-01-29 18:40:32 +0100

    feat: display message when there is no data in expenses pie chart

--- components/dashboard/expenses-by-category-card.tsx
+++ components/dashboard/expenses-by-category-card.tsx
@@ -1,5 +1,8 @@
 import { Category } from "@/types/types";
+import { FontAwesome } from "@expo/vector-icons";
 import { UseQueryResult } from "@tanstack/react-query";
+import { useRouter } from "expo-router";
+import isEmpty from "lodash/isEmpty";
 import map from "lodash/map";
 import orderBy from "lodash/orderBy";
 import size from "lodash/size";
@@ -11,10 +14,13 @@ import { Dimensions, View } from "react-native";
 import { PieChart } from "react-native-chart-kit";
 import colors from "tailwindcss/colors";
 import { Alert, AlertIcon, AlertText } from "../ui/alert";
+import { Button, ButtonIcon, ButtonText } from "../ui/button";
 import { Card } from "../ui/card";
+import { Center } from "../ui/center";
 import { Heading } from "../ui/heading";
-import { InfoIcon } from "../ui/icon";
+import { AddIcon, InfoIcon } from "../ui/icon";
 import { Skeleton } from "../ui/skeleton";
+import { Text } from "../ui/text";
 import { VStack } from "../ui/vstack";
 
 const COLOR_PALETTE = [
@@ -33,6 +39,7 @@ export default function ExpensesByCategoryCard({
   categories,
 }: Readonly<ExpensesByCategoryCardProps>) {
   const { t } = useTranslation();
+  const { push } = useRouter();
 
   const { data: categoriesData, isLoading, isError } = categories;
 
@@ -79,18 +86,44 @@ export default function ExpensesByCategoryCard({
                   </Alert>
                 </Then>
                 <Else>
-                  <PieChart
-                    data={fiveMostUsedCategories}
-                    width={Dimensions.get("window").width - 24}
-                    height={220}
-                    chartConfig={{
-                      color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
-                      useShadowColorFromDataset: false,
-                    }}
-                    accessor="expenseCount"
-                    backgroundColor="transparent"
-                    paddingLeft="0"
-                  />
+                  <If condition={!isEmpty(fiveMostUsedCategories)}>
+                    <Then>
+                      <PieChart
+                        data={fiveMostUsedCategories}
+                        width={Dimensions.get("window").width - 24}
+                        height={220}
+                        chartConfig={{
+                          color: (opacity = 1) =>
+                            `rgba(26, 255, 146, ${opacity})`,
+                          useShadowColorFromDataset: false,
+                        }}
+                        accessor="expenseCount"
+                        backgroundColor="transparent"
+                        paddingLeft="0"
+                      />
+                    </Then>
+                    <Else>
+                      <Center>
+                        <VStack space="md" className="items-center">
+                          <FontAwesome
+                            name="pie-chart"
+                            size={50}
+                            color={colors.neutral[300]}
+                          />
+                          <Text
+                            size="sm"
+                            className="px-20 text-center text-typography-400"
+                          >
+                            {t("Empty categories")}
+                          </Text>
+                          <Button onPress={() => push("/expenses/create")}>
+                            <ButtonIcon as={AddIcon} />
+                            <ButtonText>{t("Create new expense")}</ButtonText>
+                          </Button>
+                        </VStack>
+                      </Center>
+                    </Else>
+                  </If>
                 </Else>
               </If>
             </Else>
